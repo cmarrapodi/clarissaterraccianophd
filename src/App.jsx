c09aeea094cecb1cab7adb2cac1c37d1d2b70eb1
cmarrapodi <158534253+cmarrapodi@users.noreply.github.com>
2026-05-29 16:13:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ClarissaWebsite from './ClarissaWebsite'
+import AdminLogin from './pages/AdminLogin'
+import AdminDashboard from './pages/AdminDashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClarissaWebsite />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
